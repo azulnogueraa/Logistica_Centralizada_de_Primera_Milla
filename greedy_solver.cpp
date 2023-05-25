a15bd034fa3ec06d9120f76932dfdc3e0bb77092
azulnogueraa <101016508+azulnogueraa@users.noreply.github.com>
2023-05-25 17:56:51 -0300

    Update greedy_solver.cpp

--- greedy_solver.cpp
+++ greedy_solver.cpp
@@ -18,32 +18,57 @@ void GreedySolver::setInstance(TaxiAssignmentInstance &instance) {
 
 void GreedySolver::solve() {
 
-    //auto solucion = TaxiAssignmentSolution(n);
+    //inicializamos el cronometro que contara el tiempo de resolución Greedy...
+    auto start = chrono::steady_clock::now();
+
+
     for (int p = 0; p < this->_instance.n; p++){
-        
+
+        //taxi con menor distancia a p
         int min_taxi = -1;
 
-        //buscamos el primer taxi sin asignar dentro de los restantes para cada pasajero...
+        //buscamos cualquier taxi sin asignar dentro de los restantes para cada pasajero y se lo asignamos temporalmente a min_taxi...
         while(min_taxi == -1){
+
             for(int t = 0; t < this->_instance.n; t++){
+
+                //verificamos que el taxi no haya sido asignado a ningun otro pasajero...
                 if(this->_solution.isTaxiAssigned(t) == false){
                     min_taxi = t;
                 }
             }
+
         }
 
-
-
-        // double min = this->_instance.dist[0][p];
+        double min_dist = this->_instance.dist[min_taxi][p];
         
-        // for (int t = 0; t < this->_instance.n ; t++){
-        //     if ((this->_instance.dist[t][p] < min) && (!this->_solution.isTaxiAssigned(t))){
-        //         min = this->_instance.dist[t][p];
-        //         min_taxi = t;
-        //     }
-        // }
-        // this->_solution.assign(min_taxi,p);
+        for (int t = 0; t < this->_instance.n ; t++){
+
+            //si la distancia de un taxi al pasajero p es menor a min_dist y el taxi no esta asignado, cambiamos las variables...
+            if ((this->_instance.dist[t][p] < min_dist) && (!this->_solution.isTaxiAssigned(t))){
+                min_dist = this->_instance.dist[t][p];
+                min_taxi = t;
+            }
+
+        }
+
+        //guardamos para cada pasajero el taxi mas cercado (FCFS)...
+        this->_solution.assign(min_taxi,p);
+
+        //el valor objetivo equivale a la suma de las distancias recorridas por cada taxi hasta su pasajero asignado...
+        this->_objective_value += this->_instance.dist[min_taxi][p];
+
     }
+
+    //terminamos el cronometro...
+    auto end = chrono::steady_clock::now();
+
+    //tiempo de resolución equivale a: end - start
+    chrono::duration<double, std::milli> duration = end - start;
+    this->_solution_time = duration.count();
+
+    //bibliografia de chrono: https://openwebinars.net/blog/como-usar-la-libreria-chrono-en-c/
+
 }
 
 double GreedySolver::getObjectiveValue() const {
